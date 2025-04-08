ecd3db5d74c77d4294b00ce83ef050e3b098d045
Manthan6121 <manthan6121@gmail.com>
2025-04-08 20:28:29 +0530

    update file structure

---
+++ 2.Operators/4.5__identity.py
@@ -0,0 +1,12 @@
+"""
+
+is - True(same memory location)
+is not - True(diffrant memory location)
+
+"""
+a=[1,2,3]
+b=a
+c=[1,2,3]
+
+print(a is b)
+print(a is not c)
---
+++ 2.Operators/4.6_membership.py
@@ -0,0 +1,7 @@
+"""
+in - True(get value)
+not in - True(didn't get value)
+"""
+
+fruits= ['apple','mango','watermelon']
+print('orange' in fruits)
