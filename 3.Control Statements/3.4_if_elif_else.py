be1bf590d1d5f33ad64abfd7f43424717e75037e
Manthan6121 <manthan6121@gmail.com>
2025-04-09 17:17:33 +0530

    update

--- 3.Control Statements/3.4_if_elif_else.py
+++ 3.Control Statements/3.4_if_elif_else.py
@@ -1,19 +1,21 @@
-x = int(input('enter the first value:'))
-y = int(input('enter the second value:'))
+x = int(input('enter the value of x:'))
+y = int(input('enter the value of y:'))
 
 z = input('Choose an operator (+, -, *, /): ')
 
 if z == '+':
-    print(x+y)
+    print(f'addition of x & y ={x+y}')
+
 
 elif z == '-':
-    print(x-y)
+    print(f'subtraction of x & y ={x-y}')
 
 elif z == '*':
-    print(x*y)
+    print(f'multiplication of x & y ={x*y}')
+
 
 elif z == '/':
-    print(x/y)
+    print(f'division of x & y ={x/y}')
 
 else:
     print("Invalid Choice")
