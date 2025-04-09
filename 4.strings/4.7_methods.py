9f4af9dbaeb4445cd3ccbf9d8ee4a75c11b5331d
Manthan6121 <manthan6121@gmail.com>
2025-04-10 02:14:32 +0530

    update

--- 4.strings/4.7_methods.py
+++ 4.strings/4.7_methods.py
@@ -1,4 +1,13 @@
+# 1st program
 string = 'HELLO'
 
 print(string.lower())
 print(string.upper())
+print(string.capitalize())
+
+
+# 2nd program
+string2 = input('enter your full name:').upper()
+string3 = input('enter your state:').capitalize()
+print(string2)
+print(string3)
